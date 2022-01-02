6106e9b2fb5b0f93756845e00bf83b5087abf73d
Chuck <archdoodle@gmail.com>
2022-01-02 20:26:31 +0100

    FUCKING MASTERPIECE!!!!

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
 # Markdown Quiz Maker
 Twin styles to make quizes rapidly with.
 
-Both styles are in dist folder.
+## Installation
+
+Both styles are in dist folder. To make full use export with this appended in either body or head:
+
+```html
+<style>
+    window.onload = () => {
+
+
+
+        $ = x => document.querySelectorAll(x);
+
+
+
+        function green()
+            { this.classList.toggle('green') };
+        function red()
+            { this.classList.toggle('red') };
+
+
+
+        for ( const el of $('ol>li>ol>li, ol>li>ul>li, ul>li>ol>li, ul>li>ul>li') ) {
+            if (el.firstChild instanceof HTMLQuoteElement)
+                { el.addEventListener('mousedown',green); }
+            else
+                { el.addEventListener('mousedown',red); }
+        }
+
+
+
+    }
+</style>
+```
+
+## Usage
+
+1. Make a list of questions.
+2. In each question make a list of answers.
+3. Mark correct answers with blockquotes.
+
+
+
+
 
 Uses Night theme for typora as a base.
 https://github.com/typora/typora-default-themes
--- mq.js
+++ mq.js
@@ -1,12 +1,61 @@
-$ = x => document.querySelectorAll(x);
+const $ = x => document.querySelectorAll(x);
+const $$ = document.body;
 
-function green(){	this.classList.toggle('green')	};
-function red()	{	this.classList.toggle('red')	};
+window.onload = () => {
 
-for (const el of $('ol>li>ol>li, ol>li>ul>li, ul>li>ol>li, ul>li>ul>li') ) {
-	if	(el.firstChild instanceof HTMLQuoteElement)	{ el.addEventListener( 'mousedown',green	); }
-	else											{ el.addEventListener( 'mousedown',red		); }
+	
+
+
+	$$.innerHTML = $$.innerHTML.replace(new RegExp(	"<span class=\"cm-string\">'''"	,'g'),'<input data-ok="');
+	$$.innerHTML = $$.innerHTML.replace(new RegExp(	"'''</span>"					,'g'),'">');
+	
+	
+	for ( const el of $('ol>li>ol>li, ol>li>ul>li, ul>li>ol>li, ul>li>ul>li') ) {
+		if (el.firstChild instanceof HTMLQuoteElement)	{ el.addEventListener('mousedown',green); }
+		else											{ el.addEventListener('mousedown',red);   }
+		
+	}
+	
+	for ( const el of $('pre input, pre * input') ) {
+		el.addEventListener('input',check);
+	}
+
+	
+	for ( const el of $('input, ol>li>ol, ol>li>ul, ul>li>ol, ul>li>ul') ) {
+		el.setAttribute('tabindex', '1');
+		el.addEventListener('keydown',klik)
+	}
 }
+
+function green()	{ this.classList.toggle('green');	}
+function red()		{ this.classList.toggle('red');  	}
+function check() {
+	let v = this.value
+	let k = this.dataset.ok
+	if 		(v == '')	{	this.classList.remove('red');		this.classList.remove('green');		}
+	else if (v == k )	{	this.classList.remove('red');		this.classList.add('green');		}
+	else 				{	this.classList.add('red');			this.classList.remove('green');		}
+}
+function klik() {
+	let dic = {
+		'1':0, 'a':0,
+		'2':1, 'b':1,
+		'3':2, 'c':2,
+		'4':3, 'd':3,
+		'5':4, 'e':4,
+		'6':5, 'f':5
+	}
+	let el = $(':focus > li')[dic[event.key]];
+	if (el.firstChild instanceof HTMLQuoteElement)	{ el.classList.toggle('green');	 }
+	else											{ el.classList.toggle('red');    };
+}
+
+	// let focusable = $('pre input, pre * input, ol>li>ol, ol>li>ul, ul>li>ol, ul>li>ul');
+	// for (var i = 0, len = focusable.length; i < len; i++){
+	// 	focusable[i].setAttribute('tabindex', '0');
+	// }
+
+
 	
 	// for (const el of $('ol>li>ol>li, ol>li>ul>li, ul>li>ol>li, ul>li>ul>li, code, tt, var, kbd, pre') ) {
 	// 	el.addEventListener(
