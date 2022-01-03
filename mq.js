7236efd82cf4df8f8f9ac1d825cdb94195ecfcc9
Chuck <archdoodle@gmail.com>
2022-01-03 01:30:40 +0100

    Master Fucking Piece

--- mq.js
+++ mq.js
@@ -21,54 +21,56 @@ window.onload = () => {
 		el.addEventListener('input',check);
 	}
 
-	
-	for ( const el of $('input, ol>li>ol, ol>li>ul, ul>li>ol, ul>li>ul') ) {
+	const focusable = $('input, ol>li>ol, ol>li>ul, ul>li>ol, ul>li>ul')
+	for ( const el of focusable ) {
 		el.setAttribute('tabindex', '1');
 		el.addEventListener('keydown',klik)
 	}
+	focusable[0].focus()
 }
 
-function green()	{ this.classList.toggle('green');	}
-function red()		{ this.classList.toggle('red');  	}
+function green()	{ this.classList.toggle('green');	count();	}
+function red()		{ this.classList.toggle('red');  	count();	}
 function check() {
 	let v = this.value
 	let k = this.dataset.ok
 	if 		(v == '')	{	this.classList.remove('red');		this.classList.remove('green');		}
 	else if (v == k )	{	this.classList.remove('red');		this.classList.add('green');		}
 	else 				{	this.classList.add('red');			this.classList.remove('green');		}
+	count();
 }
 function klik() {
-	let dic = {
-		'1':0, 'a':0,
-		'2':1, 'b':1,
-		'3':2, 'c':2,
-		'4':3, 'd':3,
-		'5':4, 'e':4,
-		'6':5, 'f':5
+	let e = $(':focus > li');
+	let k = event.key;
+	if (
+		k == '1' 	||		k == '2'	||		k == '3'	||		k == '4'	||		k == '5'	||		k == '6'	||
+		k == 'a' 	||		k == 'b'	||		k == 'c'	||		k == 'd'	||		k == 'e'	||		k == 'f'
+	) {
+
+		let d = {
+			'1':0,			'2':1,				'3':2,				'4':3,				'5':4,				'6':5,
+			'a':0,			'b':1,				'c':2,				'd':3,				'e':4,				'f':5
+		}
+		let v = d[k];
+		if (v < e.length) {
+			
+			let el = e[v];
+			
+			if (el.firstChild instanceof HTMLQuoteElement)	{ el.classList.toggle('green');	 }
+			else											{ el.classList.toggle('red');    };
+			count();
+			
+		}
 	}
-	let el = $(':focus > li')[dic[event.key]];
-	if (el.firstChild instanceof HTMLQuoteElement)	{ el.classList.toggle('green');	 }
-	else											{ el.classList.toggle('red');    };
 }
 function count() {
-	counter.innerHTML = $('.green') - $('.red')
+	let x = $('.green').length / ($('.green').length + $('.red').length) * 100
+	counter.innerHTML = ( x ) + '%';
+	if ( x < 70 )	{	counter.classList.add('red');			counter.classList.remove('green');		}
+	else			{	counter.classList.remove('red');		counter.classList.add('green');		}
 }
-	// let focusable = $('pre input, pre * input, ol>li>ol, ol>li>ul, ul>li>ol, ul>li>ul');
-	// for (var i = 0, len = focusable.length; i < len; i++){
-	// 	focusable[i].setAttribute('tabindex', '0');
-	// }
 
 
-	
-	// for (const el of $('ol>li>ol>li, ol>li>ul>li, ul>li>ol>li, ul>li>ul>li, code, tt, var, kbd, pre') ) {
-	// 	el.addEventListener(
-	// 		'mousedown',() => {
-	// 			if(window.event.button==2){
-	// 				copyToClp(window.event.target.innerHTML)
-	// 			}
-	// 		}
-	// 	)
-	// }
 
 // function copyToClp(txt){
 // 	if (window.clipboardData && window.clipboardData.setData) {
